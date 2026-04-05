bcfc8acac0c9bded23f69adc325d2ec8dc895ffd
Eden <157859596+DlonRash@users.noreply.github.com>
2026-04-05 00:21:07 -0500

    Pygame Implementation
    Adding screen setup.

--- Knight_Game.py
+++ Knight_Game.py
@@ -1,1 +1,28 @@
-Dlon
+## Board Game Script[D'lon Rash]
+
+# since im gonna want to visualize the project, I'm going to start implementing pygame to help me do so. "Dlon"
+
+import pygame
+# screen setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("Grey")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()    
+    
