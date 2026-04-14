58fd080d1b5cc4b438f19ea8109dc7992a70eba3
Chris <christopherboy06@gmail.com>
2026-04-13 21:10:20 -0500

    all 3 of our stuff in one.

--- Knight_Game.py
+++ Knight_Game.py
@@ -39,6 +39,163 @@ while running:
 pygame.quit()    
     
 
+
+
+
+"""Kara's code for our project: """
+"""For the abbrivations:
+ele = elements
+(word)Sq = currant (something)
+    aka: curSq = currant Sqaure
+"""
+
+def get_moves(position):
+    x,y = position
+    #all 8 possible moves:
+    moves_pos = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+
+    valid_m= []
+
+    for j,k in moves_pos:
+        new_x, new_y = x + j, y + k
+        
+        #keeping the variables in the board:
+        if 0 <= new_x <8 and 0 <= new_y <8:
+            valid_m.append((new_x,new_y))
+    return valid_m
+
+
+#choose your starting position:
+x = int(input("Enter an x: "))
+if x< 0 or x> 7:
+    while x <0 or x >7:
+        x = int(input("enter a number between 0 and 7:"))
+
+
+y = int(input("Enter an y: "))
+if 0 > y or y > 7:
+       while y <0 or y >7:
+        y = int(input("enter a number between 0 and 7:")) 
+
+#choose the target
+tx = int(input("Enter an x for target: "))
+if tx< 0 or tx> 7:
+    while tx <0 or tx >7:
+        tx = int(input("enter a number between 0 and 7:"))
+
+ty = int(input("Enter an y for target: "))
+if 0 > ty or ty > 7:
+       while ty <0 or ty >7:
+        ty = int(input("enter a number between 0 and 7:")) 
+
+
+
+startPos = [x,y]
+target = [tx, ty]
+
+startPos = tuple(startPos)
+target = tuple(target)
+
+
+#BFS
+def BFS():
+
+#seting the lists togethers   
+    queue = [[startPos]]
+    visitSq = {startPos}
+
+#traveling throughtout the board:
+    while len(queue) != 0:
+        curPath = queue.pop(0)
+        curSq = curPath[-1]
+        """print(curSq)"""
+
+        if curSq == target:
+            return curPath
+
+        for ele in get_moves(curSq):
+            '''print(ele)'''
+            #if news positions aren't in the visitSq
+            if ele not in visitSq:
+                visitSq.add(ele)
+
+                queue.append(curPath+ [ele])
+
+#DFS
+
+"""Just converted the BFS into a DFS by poppin the 1 item"""
+def DFS():
+
+#seting the lists togethers   
+    queue = [[startPos]]
+    visitSq = {startPos}
+
+#traveling throughtout the board:
+    while len(queue) != 0:
+        curPath = queue.pop()
+        curSq = curPath[-1]
+        """print(curSq)"""
+
+        if curSq == target:
+            return curPath
+
+        for ele in get_moves(curSq):
+            '''print(ele)'''
+            #if news positions aren't in the visitSq
+            if ele not in visitSq:
+                visitSq.add(ele)
+
+                queue.append(curPath+ [ele])
+
+
+
+def aSearching():
+#checking to if target is the currant square
+    if startPos == target:
+        return [startPos]
+
+   
+    seen = {startPos}
+    # [score, path]
+    q = [[0, [startPos]]]
+
+    while q:
+        q.sort() 
+#storing the values
+        _, path = q.pop(0) 
+        
+        curr = path[-1]
+
+        for ele in get_moves(curr):
+            if ele == target:
+                return path + [ele]
+            
+            if ele not in seen:
+                seen.add(ele)
+                h = (abs(tx - ele[0]) + abs(ty - ele[1])) / 3
+                g = len(path)
+                
+                q.append([g + h, path + [ele]])
+    
+        
+
+
+
+
+
+
+a = BFS()
+print(a)
+
+b = DFS()
+print(b)
+
+c = aSearching()
+print(c)
+            
+
+
+
 #tree stuff Chris
 class tree:
     def __init__(self, size=8): #cause it's 8*8
@@ -59,15 +216,21 @@ class tree:
                 print(f'{self.board[row-1][col]:2}', end='')
             print()
     
-    def set_marker(self, row, col, value='g'):
+    def set_marker(self, row, col, value=' g'):
         if row < 1 or row > 8 or col < 1 or col > 8:
             print("please use numbers between 1 and 8")
             return
         self.board[row-1][col-1] = value
 
-    def set_knight(self, row, col, value='k'):
+    def set_knight(self, row, col, value=' k'):
         if row < 1 or row > 8 or col < 1 or col > 8:
             print("please use numbers between 1 and 8")
             return
         self.board[row-1][col-1] = value
 
+
+
+p = tree()
+p.set_marker(tx, ty)   #setting a marker/goal at 4,6
+p.set_knight(x, y)    #setting the knight at 5,4
+p.print_board()
